f9443d846510dadb14d5d3c815b0b27045e92b4d
Artem Prudnikov <artemprudnikov907@gmail.com>
2024-04-29 18:13:50 +0300

    Create the TodoForm with form with an input and button with state text for input

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import './App.css';
-
+//Импорт компонента TodoForm
+import TodoForm from './components/TodoForm';
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Todo application</h1>
+      <TodoForm />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+function TodoForm() {
+  // Состояние хранящее в себе содержимое инпута
+  const [text, setText] = useState('');
+
+  //Обработчик для form
+  function onSubmitHandler(event) {
+    //Чтобы страница не обновлялась
+    event.preventDefault();
+    //Чтобы при submit инпут очищался
+    setText('');
+  }
+  return (
+    <form onSubmit={onSubmitHandler}>
+      <input
+        type="text"
+        name="todo input"
+        // Значение поля ввода привязано к состоянию "text", чтобы отслеживать изменения в этом поле.
+        value={text}
+        // При изменении текста вызывается функция setText, которая обновляет состояние "text"
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+export default TodoForm;
