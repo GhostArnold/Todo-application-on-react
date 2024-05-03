c3ebfabcbc309c3973fa1f98f59bc11ed4a1d597
Artem Prudnikov <artemprudnikov907@gmail.com>
2024-05-03 13:24:46 +0300

    The finished version of TodoApp2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+// Уникальный id
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 //Импорт модуля css для App
 import styles from './App.module.css';
@@ -6,33 +8,73 @@ import styles from './App.module.css';
 import TodoForm from './components/TodoForm';
 // Импорт компонента TodoList
 import TodoList from './components/TodoList';
+//Импорт компонента для удаления всего и удаления всех отмеченных задач
+import TodosActions from './components/TodosActions';
 function App() {
   //Создаём состояние массива, хранящего в себе текст из субмита
   const [todos, setTodos] = useState([]);
 
   //Создаём обработчик для добавления элементов в массив. text - новый элемент
   const addTodoHandler = (text) => {
+    //Объект, который предсставляет из себя элементы массива
+    const newTodo = {
+      //Текст
+      text: text,
+      //Завершенно или нет
+      isCompleted: false,
+      //Айдишник
+      id: uuidv4(),
+    };
     // ...todos - перебор уже существующих элементов в массиве
-    setTodos([...todos, text]);
+    setTodos([...todos, newTodo]);
   };
 
   // Создаём обработчик удаления
-  // index - параметр в который мы в дальнейшем засунем индекса элементов массивов
-  const deleteTodoHandler = (index) => {
-    // idx представляет индекс текущего элемента в массиве todos. _ - затычка, вместо массива todos, т.к мы его не используем
-    //Это всё шаблонные параметры
-    // idx - текущий элемент, который мы получаем при клике
-    setTodos(todos.filter((_, idx) => index !== idx));
+  const deleteTodoHandler = (id) => {
+    //Мы делаем итерацию по todo.id в объектах массива и сравниваем с тем id, который передаётся в качетсве параметра функции
+    setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  // Обработчик для завешённых задач
+  const toggleTodoHandler = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        // Если мы нашли todo.id который совпадает с id при вызове функции то..
+        // Подход с созданием нового объекта называется иммутабельностью данных и он часто используется в React и других JavaScript-проектах для предотвращения нежелательных изменений состояния
+        return todo.id === id
+          ? // Если условие из пункта 1 истинно, то создается новый объект задачи, копируя все свойства текущей задачи (...todo), а затем обновляя свойство isCompleted на противоположное значение с помощью !todo.isCompleted. Например, если isCompleted было false, теперь оно станет true, и наоборот.
+            { ...todo, isCompleted: !todo.isCompleted }
+          : // Иначе всё остаётся так же и объект пересоздаётся
+            { ...todo };
+      })
+    );
+  };
+
+  //Обработчик для удаления завершённых задач
+  const deleteCompletedHandler = () => {
+    setTodos(todos.filter((todo) => !todo.isCompleted));
+  };
+
+  const resetHandler = () => {
+    setTodos([]);
+  };
   return (
     <div className="App">
       <div className={styles.mainBlock}>
         <h1>Todo application</h1>
         {/* addTodoHAndler - передаём обработчик */}
         <TodoForm addTodo={addTodoHandler} />
+        <TodosActions
+          todos={todos}
+          deleteCompleted={deleteCompletedHandler}
+          reset={resetHandler}
+        />
         {/* Передаём массив */}
-        <TodoList todos={todos} deleteTodo={deleteTodoHandler} />
+        <TodoList
+          todos={todos}
+          deleteTodo={deleteTodoHandler}
+          toggleTodo={toggleTodoHandler}
+        />
       </div>
     </div>
   );
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,27 @@
+import { RiDeleteBack2Line } from 'react-icons/ri';
+import { FaCheck } from 'react-icons/fa';
 import styles from './Todo.module.css';
-function Todo({ todo, index, deleteTodo }) {
+function Todo({ todo, deleteTodo, toggleTodo }) {
   // Выводим todo
   return (
-    <div className={styles.todo} onDoubleClick={() => deleteTodo(index)}>
+    <div
+      className={`${styles.todo} ${
+        todo.isCompleted ? styles.completedTodo : ''
+      }`}
+    >
       {/* Передаём калбэк в событие двойного клика и там вызываем фильтрацию, куда пишем параметр индекса при переборе*/}
-      <div className={styles.todoItem}>{todo}</div>
+      <div className={styles.todoItem}>{todo.text}</div>
+      <div className={styles.icons}>
+        <RiDeleteBack2Line
+          className={styles.deleteIcon}
+          // Удаление по клику
+          onClick={() => deleteTodo(todo.id)}
+        />
+        <FaCheck
+          className={styles.completeIcon}
+          onClick={() => toggleTodo(todo.id)}
+        />
+      </div>
     </div>
   );
 }
